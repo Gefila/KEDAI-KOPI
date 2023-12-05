3b3479ca5598a417d5814b05ff4aefbae9baf63f
Gefila <106391467+Gefila@users.noreply.github.com>
2023-12-06 00:24:00 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ document.getElementById("hamburger-menu").addEventListener("click", ()=>{
 })
 
 
+// klik di luar sidebar untku menghilangkan nav
+const hamburger= document.getElementById("hamburger-menu");
+document.addEventListener('click',(e)=>{
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove("active")
+    }
+})
